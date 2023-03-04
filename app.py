9a1d6220f662b47013c4d4e5a598138a5c48ae50
khaledCSE <khaledcse10@gmail.com>
2023-03-05 05:26:46 +0600

    CORS support and basic html

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import pickle
 import numpy as np
 from flask import Flask, request, json, jsonify
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/", methods=["POST"])
+@cross_origin()
 def post_comment():
     # Taking JSON input
     jsonData = request.get_json(force=True)
